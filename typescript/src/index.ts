e47af254a38d2dfba027e081f62aa603d0cf1d01
Will Squibb <wsquibb@andrew.cmu.edu>
2023-09-04 17:38:26 -0400

    Shape = Rectange(2,3)

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -3,6 +3,6 @@ import { newCircle } from "./shapes/circle.js";
 import { newRectangle } from "./shapes/rectangle.js";
 import { newSquare } from "./shapes/square.js";
 
-const shape: Shape = newSquare(1)
+const shape: Shape = newRectangle(2, 3)
 const renderer = newRenderer(shape)
 renderer.draw();
